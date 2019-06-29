ede3bb75ae9b01bbbdba8b32e51abf18cf081558
Jonathan Sees <jonathan.m.sees@gmail.com>
2019-06-28 22:57:59 -0700

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -18,12 +18,28 @@ router.use("/signup", signupRoute);
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    router.use(function(req, res) {
+      res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    });
+
+    // app.use(express.static(path.join(__dirname, 'client/build')));
         
     // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
+    router.get('*', function(req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 }
+
+
+
+// if (process.env.NODE_ENV === 'production') {
+//     // Serve any static files
+//     app.use(express.static(path.join(__dirname, 'client/build')));
+        
+//     // Handle React routing, return all requests to React app
+//     app.get('*', function(req, res) {
+//         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//     });
+// }
 
 module.exports = router;
