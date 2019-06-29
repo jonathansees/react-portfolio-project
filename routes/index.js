62dca9544cd928fb30d173a1492f184f0dfbdb05
Jonathan Sees <jonathan.m.sees@gmail.com>
2019-06-28 22:50:17 -0700

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const signinRoute = require("./auth/signin");
 const signupRoute = require("./auth/signup");
+const express = require('express');
 const app = express();
 
 // API Routes
