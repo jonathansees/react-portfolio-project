aaa6550a8828a44078646f57ef716455b4426950
Jonathan Sees <jonathan.m.sees@gmail.com>
2019-06-28 23:20:17 -0700

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -12,34 +12,8 @@ router.use("/signin", signinRoute);
 router.use("/signup", signupRoute);
 
 // If no API routes are hit, send the React app
-// router.use(function(req, res) {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
-
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    // router.use(function(req, res) {
-    //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    // });
-
-    // app.use(express.static(path.join(__dirname, 'client/build')));
-        
-    // Handle React routing, return all requests to React app
-    router.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-}
-
-
-
-// if (process.env.NODE_ENV === 'production') {
-//     // Serve any static files
-//     app.use(express.static(path.join(__dirname, 'client/build')));
-        
-//     // Handle React routing, return all requests to React app
-//     app.get('*', function(req, res) {
-//         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-//     });
-// }
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
 
 module.exports = router;
