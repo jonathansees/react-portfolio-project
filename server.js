1bd9d449cd57f8c8ba74d7ed3590cfdb4a7d3b37
Jonathan Sees <jonathan.m.sees@gmail.com>
2019-07-01 12:17:10 -0700

    initial commit

--- server.js
+++ server.js
@@ -1,29 +1,64 @@
-//Main starting point of our app
 const express = require('express');
-const path = require('path');
-const http = require('http');
 const bodyParser = require('body-parser');
+const path = require('path');
 const morgan = require('morgan');
-const app = express();
-const routes = require('./routes');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-// DB Setup
-mongoose.connect('mongodb://localhost:auth/auth', { 
-    useCreateIndex: true,
-    useNewUrlParser: true
-});
+const app = express();
+const port = process.env.PORT || 5000;
 
 // App Setup
 app.use(morgan('combined'));
 app.use(cors());
 app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(routes);
+// API calls
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
 
-// Server Setup
-const port = process.env.PORT || 5000;
-const server = http.createServer(app);
-server.listen(port);
-console.log('Server listening on:', port);
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
+
+const yelp = require('yelp-fusion');
+const apiKey = 'xvNpInpIU1FBjlSOg9yEVRptavdvJhcpVh6eIWM4maRwPpfctooKafFos_M77m9iBRqvPd3N9Fs7Ts8wbI5Q1zVxdEtWkAQE9BxLQwef6STO66hcOEunNuSNlaIIXHYx';
+const client = yelp.client(apiKey);
+
+// Matches with "/api/restaurants"
+app.get('/api/restaurants', (req, res) =>{
+    const {query} = req
+    console.log(query)
+    const searchRequest = {
+      term: query.searchTerm,
+      location: query.searchLocation,
+      price: "1",
+      open_now: (query.searchOpen ? true : false)
+    };
+
+    client.search(searchRequest).then(response => {
+      // const id = response.jsonBody.businesses[0].id;
+      // console.log(id);
+      res.json(response);
+    }).catch(e => {
+      console.log(e);
+    });
+});
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+    
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
